e685a02ecee43c3be452b9a8c1a6507a7b457ea2
pixie_dust <scmgoes@hotmail.com>
2021-12-11 18:41:07 +0000

    update

---
+++ controle/if_else_2.js
@@ -0,0 +1,8 @@
+const hora = 23;
+let saudacao;
+
+if (hora < 12) {
+    console.log(saudacao = 'Bom dia!');
+} else {
+    console.log(saudacao = 'Boa tarde!');
+}
---
+++ controle/if_else_3.js
@@ -0,0 +1,12 @@
+const hora = 17;
+let saudacao;
+
+if (hora < 12) {
+    console.log(saudacao = 'Bom dia!');
+}
+
+if (hora >= 19) {
+    console.log(saudacao = 'Boa noite')
+} else {
+    console.log(saudacao = 'Boa tarde!');
+}
