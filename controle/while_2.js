aeccd191c57f257821356e00016b54b122cb3636
pixie_dust <scmgoes@hotmail.com>
2021-12-11 22:40:51 +0000

    update

--- controle/while_2.js
+++ controle/while_2.js
@@ -1,1 +1,8 @@
-console.log(Math.random());
+let quantidade = 0;
+
+while (Math.random() < 0.8) { //ENQUANTO ESTA EXPRESSÃO FOR VERDADEIRA EXECUTE
+    console.log('< 0.8');
+    quantidade++;
+}
+
+console.log('Quantidade: ' + quantidade);
