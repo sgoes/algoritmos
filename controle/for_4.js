7be865a43f5fb37d3308271cc948c671e67a6b54
pixie_dust <scmgoes@hotmail.com>
2021-12-17 21:38:15 +0000

    update

--- controle/for_4.js
+++ controle/for_4.js
@@ -2,4 +2,22 @@
 // [2, 1], [2, 2], [2, 3] // o i aumentou de 1 para dois 
 // [3, 1], [3, 2], [3, 3]
 
-//vamos usar um laço for para imprimir o esquema antereior, vamos precisar de dois laços
+//vamos usar um laço for para imprimir o esquema antereior, vamos precisar de dois laços 
+for (let i = 1; i <= 3; i++) {
+    let linha = '';
+    for (let j = 1; j <= 3; j++) {
+        linha += `[${i}, ${j}] `;
+    }
+    console.log(linha);
+}
+
+//vamos supor que não quero exibir quando os numeros são iguais, preciso de criar uma outra estrutura de controle do tipo IF
+for (let i = 1; i <= 3; i++) {
+    let linha = '';
+    for (let j = 1; j <= 3; j++) {
+        if (i !== j) { //se o i for estreitamente diferente de j
+            linha += `[${i}, ${j}] `;
+        }
+    }
+    console.log(linha);
+}
