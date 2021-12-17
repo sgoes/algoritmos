969985be65e5b1211830d993ab65bb374652f395
pixie_dust <scmgoes@hotmail.com>
2021-12-17 14:04:19 +0000

    new files

---
+++ controle/for_2.js
@@ -0,0 +1,28 @@
+//for(;;){} //o declarar a variavel, colocar a expressao e colocar o incrimento são partes opcionais do ciclo for
+
+//for(; true;){
+//}
+
+
+//exemplo de outra forma para para fazer um ciclo for 
+//let controle = 1; //declaramos aqui a variavel
+//for (; controle <= 10;) { //declaramos a expressao
+//    console.log(controle); //mandamos imprimir o resultado
+//    controle++; //a aqui adicionamos o incrimento
+//}
+//console.log('Fim');
+
+
+//para passar para um ciclo While, basta retirar os pontos e virgulas
+let controle = 1; //declaramos aqui a variavel
+while (controle <= 10) { //declaramos a expressao
+    console.log(controle); //mandamos imprimir o resultado
+    controle++; //a aqui adicionamos o incrimento
+}
+
+console.log('FIM');
+
+//mas isto não a forma mais vulgar
+
+
+//criar laço infinito for
---
+++ controle/for_3.js
@@ -0,0 +1,5 @@
+//gerar um laço for que imprima na consola 7, 6, 5, 4, 3
+
+for (let i = 7; i >= 3; i--) {
+    console.log(i);
+}
