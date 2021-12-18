328a0a5bf5dca55899dce9a8a0b3180c466100e5
pixie_dust <scmgoes@hotmail.com>
2021-12-18 22:43:46 +0000

    up

---
+++ funcoes/extra_undefined.js
@@ -0,0 +1,7 @@
+let a; //variavel nao inicializada
+let b = 7;
+let c = null; // ou nil em algumas linguagens
+let d = undefined;
+
+console.log(a, b, c, d);
+console.log(a + b);
---
+++ funcoes/funcao_e_undefined.js
@@ -0,0 +1,10 @@
+function semRetorno() {
+    console.log('Função foi chamada!');
+}
+
+let a = 3;
+let b = a * 7 + 10; //tenho aqui uma expressao que está inicializando a variavel b
+let c = semRetorno(); //peguei na resposta de uma função e atribui a uma variavel que
+let d;
+
+console.log(a, b, c, d);
