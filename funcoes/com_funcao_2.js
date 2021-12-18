7a8a35c8b097a22e679dd2bdf8693c319d6f0379
pixie_dust <scmgoes@hotmail.com>
2021-12-18 19:35:16 +0000

    up

--- funcoes/com_funcao_2.js
+++ funcoes/com_funcao_2.js
@@ -17,5 +17,10 @@ function exibirData2() {
     console.log(`${d2}-${m2}-${a2}`);
 }
 
+function exibirData(d, m, y) {
+    console.log(`${d}--${m}--${y}`);
+}
+
+exibirData(d1, m1, a1)
 exibirData1();
 exibirData2();
---
+++ funcoes/desafio_funcao_1.js
@@ -0,0 +1,18 @@
+// funcao: 2 params --> console.log(+)
+// funcao: 2 parms -> console.log(-)
+
+
+
+function soma(n1, n2) {
+    console.log(n1 + n2);
+}
+
+soma(3, 4);
+soma(31, 98);
+
+function subtracao(n1, n2) {
+    console.log(n1 - n2);
+}
+
+subtracao(11, 7);
+subtracao(19, 52);
